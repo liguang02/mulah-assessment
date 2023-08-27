4dd4b7804333cd57252fc50d0d19b4c88f89683a
LiGuangChai <lcha0068@student.monash.edu>
2023-08-27 20:58:07 +0800

    completed mulah task

--- mulah-assessment.js
+++ mulah-assessment.js
@@ -1,5 +1,19 @@
 let express = require('express');
+let fs = require("fs");
+const {parse} = require("csv-parse");
+
 let app = express();
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
+db = [];
+fs.createReadStream("Table_Input.csv")
+    .pipe(parse({delimiter: ",", from_line: 2}))
+    .on("data", (row) => {db.push(row);})
+    .on("error", (err) => {console.log(err.message);})
+    .on("end", () => {console.log("done");});
 app.get("/", function(req, res, next) {
-  res.send("Hello world");
+//   res.send(db[1][0]);
+  res.render('app.html', {data: db});
+  
 }).listen(8080);
